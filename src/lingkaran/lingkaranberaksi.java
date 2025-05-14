cdaaa223b449917ab7d68eba94c5e742a362ac2c
Rangz1727 <pandurangga757@gmail.com>
2025-05-14 16:55:52 +0700

    Add files via upload

---
+++ src/lingkaran/lingkaranberaksi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lingkaran;
+
+/**
+ *
+ * @author Pandu
+ */
+public class lingkaranberaksi {
+    public static void main(String[] args) {
+        lingkaran ling = new lingkaran();
+        double r = 11.78;
+        ling.hitungLuas(r);
+    }
+}
